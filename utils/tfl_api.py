24eb06b11f734a975aeebf6abb650923a8cc1007
andriikolisnyk <68229112+Doctor2007@users.noreply.github.com>
2025-05-19 18:28:04 +0100

    Update tfl_api.py

--- utils/tfl_api.py
+++ utils/tfl_api.py
@@ -120,7 +120,7 @@ if __name__ == '__main__':
     logger.remove()
     logger.add("logs/tfl_api.log", level="INFO", mode="w")
 
-    API_KEY = '090c6690b1b34994aaab734b8fc14cd3'
+    API_KEY = 'your_api_key'
 
     n_errors = 0
 
